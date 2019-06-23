33723a9f4603d5dbc6185b31eb12ed16a87527f1
二熊不是熊二Simona <980715844@qq.com>
2019-06-23 17:12:35 +0800

    index project

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import 'babel-polyfill'
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import fastclick from 'fastclick'
+
+import 'common/stylus/index.styl'
+
+// FastClick消除点击300毫秒的延时 提高程序的运行效率
+fastclick.attach(document.body)
+
+// Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
